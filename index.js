b80cb63752f892d871c37e4ec75c5c94129ca561
Vlad Pozniak <vp.9433@gmail.com>
2023-09-15 17:50:16 +0300

    add config for vercel

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const eventsRouter = require('./routes/events');
 const clientsRouter = require('./routes/clients');
 const earningsRouter = require('./routes/earnings');
 
-const app = express();
+const index = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+index.use(logger('dev'));
+index.use(express.json());
+index.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+index.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
-app.use('/events', eventsRouter);
-app.use('/clients', clientsRouter);
-app.use('/earnings', earningsRouter);
+index.use('/events', eventsRouter);
+index.use('/clients', clientsRouter);
+index.use('/earnings', earningsRouter);
 
 // error handler
-app.use(function(err, req, res, next) {
+index.use(function(err, req, res, next) {
   res.json(err);
 });
 
@@ -33,7 +33,7 @@ const start = async () => {
     await mongoose.connect(
         "mongodb+srv://DDoS:ddos@cluster0.wj9qovo.mongodb.net/?retryWrites=true&w=majority"
     );
-    app.listen(3000, () => console.log("Server started on port 3000"));
+    index.listen(3000, () => console.log("Server started on port 3000"));
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -41,3 +41,5 @@ const start = async () => {
 };
 
 start();
+
+module.exports = index
